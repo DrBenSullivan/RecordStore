9da2893c80dda5f415a86cbbb4c85b5eb0eceb04
DrBenSullivan <ben.sullivan1992@gmail.com>
2025-01-14 11:56:20 +0000

    Add album model

---
+++ RecordStore.Core/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace RecordStore.Core
+{
+    public class Album
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public Guid ArtistId { get; set; }
+        public Guid GenreId { get; set; }
+        public DateOnly ReleaseYear { get; set; }
+    }
+}
