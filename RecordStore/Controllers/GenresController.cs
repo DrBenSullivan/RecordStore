9cd4726b102d341d781b0305eceea39a5102ad32
DrBenSullivan <ben.sullivan1992@gmail.com>
2025-01-14 15:15:55 +0000

    Add GenresController action method implementations.

--- RecordStore/Controllers/GenresController.cs
+++ RecordStore/Controllers/GenresController.cs
@@ -17,13 +17,19 @@ namespace RecordStore.Api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllGenres()
         {
-            throw new NotImplementedException();
+            var result = await _genreService.FindAllGenresAsync();
+
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetGenreById(int id)
         {
-            throw new NotImplementedException();
+            var result = await _genreService.FindGenreByIdAsync(id);
+
+            if (result == null) return NotFound($"The genre with id '{id}' could not be found.");
+
+            return Ok(result);
         }
     }
 }
