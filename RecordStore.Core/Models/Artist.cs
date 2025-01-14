da08bfa29000aa384abab08161c26bc1acb15aae
DrBenSullivan <ben.sullivan1992@gmail.com>
2025-01-14 12:30:38 +0000

    Add DbContext config for Artist model.

--- RecordStore.Core/Models/Artist.cs
+++ RecordStore.Core/Models/Artist.cs
@@ -4,11 +4,7 @@ namespace RecordStore.Core.Models
 {
     public class Artist
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
-        [MaxLength(255)]
         public string Name { get; set; } = string.Empty;
         public virtual List<Album> Albums { get; set; } = [];
     }
