90f70898445065a5335148a70c0dc73e5c701f59
DrBenSullivan <ben.sullivan1992@gmail.com>
2025-01-14 16:44:11 +0000

    Add CreateAlbumDto with mapping function

---
+++ RecordStore.Application/Dtos/CreateAlbumDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RecordStore.Core.Models;
+
+namespace RecordStore.Application.Dtos
+{
+    public class CreateAlbumDto
+    {
+        [Required]
+        [Length(1, 255)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public int ArtistId { get; set; }
+
+        [Required]
+        public int ReleaseYear { get; set; }
+
+        public int? GenreId { get; set; }
+
+        public Album ToAlbum() => new()
+        {
+            Title = Title,
+            ArtistId = ArtistId,
+            ReleaseYear = ReleaseYear,
+            GenreId = GenreId
+        };
+    }
+}
--- RecordStore.Core/Models/Album.cs
+++ RecordStore.Core/Models/Album.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-        public int ReleaseYear { get; set; }
         public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
         public int? GenreId { get; set; }
 
         // Navigation Properties
