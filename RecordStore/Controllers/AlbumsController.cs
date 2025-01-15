becbebcaa19bfe564b2cd74b344df69ebbaec379
DrBenSullivan <ben.sullivan1992@gmail.com>
2025-01-15 11:30:22 +0000

    Add AlbumsController.DeleteAlbum implementation

--- RecordStore/Controllers/AlbumsController.cs
+++ RecordStore/Controllers/AlbumsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
 using RecordStore.Api.Extensions;
 using RecordStore.Application.Extensions;
 using RecordStore.Core.Interfaces.ServiceInterfaces;
@@ -25,42 +26,46 @@ namespace RecordStore.Api.Controllers
             return Ok(result);
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetAlbumById(int id)
+        [HttpGet("{albumId}")]
+        public async Task<IActionResult> GetAlbumById(int albumId)
         {
-            var result = await _albumService.FindAlbumByIdAsync(id);
+            var result = await _albumService.FindAlbumByIdAsync(albumId);
 
-            if (result == null) return NotFound($"The album with id '{id}' could not be found.");
+            if (result == null) return NotFound($"The album with id '{albumId}' could not be found.");
 
             return Ok(result);
         }
 
         [HttpPost]
-        public async Task<IActionResult> PostAlbum(PostAlbumDto dto)
+        public async Task<IActionResult> PostAlbum(PostAlbumDto postAlbumDto)
         {
-            var result = await _albumService.AddAlbumAsync(dto);
+            var result = await _albumService.AddAlbumAsync(postAlbumDto);
 
-            if (result == null) return Conflict($"Unable to add album. An Album with Title '{dto.Title}', Artist Id '{dto.ArtistId}' and Release Year '{dto.ReleaseYear}' already exists.");
+            if (result == null) return Conflict($"Unable to add album. An Album with Title '{postAlbumDto.Title}', Artist Id '{postAlbumDto.ArtistId}' and Release Year '{postAlbumDto.ReleaseYear}' already exists.");
 
             return CreatedAtAction("GetAlbumById", new { result.Id }, result);
         }
 
         [HttpPut("{albumId}")]
-        public async Task<IActionResult> PutAlbum(int albumId, PutAlbumDto dto)
+        public async Task<IActionResult> PutAlbum(int albumId, PutAlbumDto putAlbumDto)
         {
-            if (dto.HasNoProperties()) return BadRequest("Unable to update album. No new property values were provided.");
+            if (putAlbumDto.HasNoProperties()) return BadRequest("Unable to update album. No new property values were provided.");
 
-            var result = await _albumService.UpdateAlbumAsync(albumId, dto);
+            var result = await _albumService.UpdateAlbumAsync(albumId, putAlbumDto);
 
             if (result == null) return NotFound($"Unable to update album. No Album with id '{albumId}' exists.");
 
             return Ok(result);
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete("{albumId}")]
         public async Task<IActionResult> DeleteAlbum(int albumId)
         {
-            throw new NotImplementedException();
+            var result = await _albumService.RemoveAlbumByIdAsync(albumId);
+
+            if (result == -1) return NotFound($"Unable to delete album. No Album with id '{albumId}' exists.");
+
+            return NoContent();
         }
     }
 }
